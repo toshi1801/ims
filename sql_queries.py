042388edd6a744c10c55ac777df9800b5cd79d65
toshi1801 <naman.jain1801@gmail.com.com>
2019-04-07 22:09:46 -0400

    Updated order table name.

--- sql_queries.py
+++ sql_queries.py
@@ -1,10 +1,10 @@
 admin_query_1 = "select p.product_id, p.product_name, p.brand, p.category, count(*) from warehouse as w, " \
                 "warehouse_product as wp, product as p where admin_id='{}' and wp.warehouse_id=w.warehouse_id and " \
-                "p.product_id=wp.product_id group by (p.product_id, p.product_name, p.brand, p.category)"
+                "p.product_id=wp.product_id group by p.product_id, p.product_name, p.brand, p.category"
 
 admin_query_2 = "select * from admin where admin_id='{}'"
 
-admin_query_3 = "SELECT budget, budget-(SELECT SUM(price) FROM placed_order WHERE admin_id='{id}' AND " \
+admin_query_3 = "SELECT budget, budget-(SELECT SUM(price) FROM orders WHERE admin_id='{id}' AND " \
                 "date_placed > date_trunc('month', current_date) GROUP BY admin_id) as remaining_balance " \
                 "FROM admin WHERE admin_id='{id}'"
 
@@ -20,14 +20,14 @@ admin_query_5 = "select ad1.street as admin_street, ad1.city as admin_city, ad1.
 
 admin_query_6 = "select v.vendor_id, v.vendor_name, po.invoice_id, po.product_id, po.price as total_cost, " \
                 "po.quantity, po.date_placed, p.product_name, p.brand, p.category, p.product_info, " \
-                "(po.price/po.quantity) as price, po.payment_status from vendor v, placed_order po, product p " \
+                "(po.price/po.quantity) as price, po.payment_status from vendor v, orders po, product p " \
                 "where v.vendor_id=po.vendor_id and po.product_id=p.product_id and admin_id='{}'"
 
 admin_query_7 = "select * from product where product_id='{}'"
 
 admin_query_8 = "select v.vendor_id, v.vendor_name, po.invoice_id, po.product_id, po.price as total_cost, " \
                 "po.quantity, po.date_placed, p.product_name, p.brand, p.category, p.product_info, " \
-                "(po.price/po.quantity) as price, po.payment_status from vendor v, placed_order po, product p " \
+                "(po.price/po.quantity) as price, po.payment_status from vendor v, orders po, product p " \
                 "where v.vendor_id=po.vendor_id and po.product_id=p.product_id and admin_id='{}' and invoice_id='{}'"
 
 admin_query_9 = "select w.warehouse_id, w.name, a.address_id, a.street, a.city, a.state, a.zip_code from warehouse w, " \
